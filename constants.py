edad4559184c8cd2b6e1d8f61a25f86c342bfac7
chloesyy <chlyiyi@hotmail.com>
2021-03-11 11:35:05 +0800

    remove unsupported tags

--- constants.py
+++ constants.py
@@ -8,6 +8,9 @@ PORT = int(os.environ.get('PORT', 5000))
 # Organisation IDs
 TIME_TO_ENTREPRET = '-432918969'
 
+# Symbols
+BULLET_POINT = '&#8226'
+
 # Messages
-START_MESSAGE = 'Hello! Welcome to the Volunteer Handbook. Here, you can ask questions or explore our libraries for information on volunteering at various organisations. \n\n Currently, this bot supports: \n &#8226 Sending questions to the adminstrators<li><ul>'
+START_MESSAGE = 'Hello! Welcome to the Volunteer Handbook. Here, you can ask questions or explore our libraries for information on volunteering at various organisations. \n\n Currently, this bot supports: \n' + BULLET_POINT + ' Sending questions to the adminstrators'
 HELP_MESSAGE = 'Here is a list of commands that you can do: \n\n /start : Start browsing! \n /help : Use this to get help!'
